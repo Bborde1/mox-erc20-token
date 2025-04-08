bff4495cbd9120dccae98e09557635d106f6ca0e
Bborde1 <Brandon.Borde@gmail.com>
2025-04-08 09:39:49 -0400

    Added fuzz tests for sub lesson contracts

---
+++ tests/sub_lesson/test_stateful.py
@@ -0,0 +1,26 @@
+from hypothesis.stateful import RuleBasedStateMachine, rule
+import pytest
+from contracts.sub_lesson import stateful_fuzz_solvable
+from hypothesis import given, HealthCheck, settings
+from boa.test.strategies import strategy
+
+
+class StatefulFuzzer(RuleBasedStateMachine):
+    def __init__(self):
+        super().__init__()
+        self.contract = stateful_fuzz_solvable.deploy()
+
+    # "Rule" -> Actions, and can have properties/invariants
+    # "Invariants" -> Properties that should always hold true
+
+    @rule(new_number=strategy("uint256"))
+    def change_number(self, new_number):
+        self.contract.change_number(new_number)
+
+    @rule(input=strategy("uint256"))
+    def input_number_returns_itself(self, input):
+        response = self.contract.always_returns_input_number(input)
+        assert response == input, f"Expected {input}, but got {response}"
+
+
+TestStatefulFuzzing = StatefulFuzzer.TestCase
